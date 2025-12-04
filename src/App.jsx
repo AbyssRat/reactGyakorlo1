24ac4e421e751bb5feba7876c581fba43f7d64a9
AbyssRat <yunothefurry@gmail.com>
2025-12-04 12:47:28 +0100

    oke szamomll

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
+{/* // növelhető, csökkenthető
+// Készíts egy számlálót, ami minden másodpercben növekszik
+// Használj useEffect-et az időzítő beállításához és takarításához */}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
 
   return (
     <>
-      
+      <div className="App">
+        <h1>Számláló: {count}</h1>
+      </div>
+
     </>
   )
 }
